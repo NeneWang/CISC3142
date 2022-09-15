9e218403c38b804c40b4c7933db9c8a4dbc6d17b
NelsonWang <wangnelson2@gmail.com>
2022-09-14 20:21:10 -0400

    ex11 |

--- lab2/ex11.cpp
+++ lab2/ex11.cpp
@@ -5,7 +5,7 @@ int main()
     int v1, v2;
     std::cout << "Enter two numbers:" << std::endl;
     std::cin >> v1 >> v2;
-    for (int idx = v1 + 1; idx < v2; idx++)
+    for (int idx = v1; idx <= v2; idx++)
     {
         std::cout << idx << std::endl;
     }
