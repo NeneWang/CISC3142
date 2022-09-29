2f1ee3d587f076ad02a4d3ce2e06265edcd9138d
NelsonWang <wangnelson2@gmail.com>
2022-09-28 20:54:18 -0400

    getting started ex 428

---
+++ lab-4/ex428.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    cout << "char" << " " << sizeof(char) << endl;
+    cout << "int" << " " << sizeof(int) << endl;
+    cout << "string" << " " << sizeof(string) << endl;
+    cout << "long" << " " << sizeof(long) << endl;
+    
+    // And so forth: https://learn.microsoft.com/en-us/cpp/cpp/fundamental-types-cpp?view=msvc-170
+}
