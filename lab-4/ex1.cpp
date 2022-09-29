2b2f7bf9a3776e830ac4f24fd78e8b3f25297560
NelsonWang <wangnelson2@gmail.com>
2022-09-28 20:37:30 -0400

    lab 4 | 1

---
+++ lab-4/ex1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+
+using namespace std;
+
+int main(){
+    vector<string> words;
+    // int COUNT_WORDS = 8;
+    string lineIn, wordFormed = "";
+    
+    getline(cin, lineIn);
+    // char character;
+    for(int idx = 0; idx < lineIn.size(); idx++){
+        // cout<<lineIn[idx];
+        // Loop for each character, when spaces or end of line found add that to the vector
+        char character = lineIn[idx];
+        wordFormed += character;
+        if(character == ' '){
+            // cout << wordFormed;
+            words.push_back(wordFormed);
+            wordFormed = "";
+
+        }
+    }
+    words.push_back(wordFormed);
+
+    string lineToPrint = "";
+    for(int idx = 0; idx < words.size(); idx++){
+        // cout << words[idx] << words.size() << endl;
+        for (auto & c: words[idx]) c = toupper(c);
+        lineToPrint += words[idx];
+        // lineToPrint += putchar(toupper(words[idx]));
+
+    }
+    cout << lineToPrint;
+
+}
+
