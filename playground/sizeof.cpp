db0aadd5100c401af45fff2dc3a5f35e89eea270
NelsonWang <wangnelson2@gmail.com>
2022-10-12 00:09:11 -0400

    calculate area

---
+++ playground/sizeof.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "char " << sizeof(char) << " bytes." << endl;
+    cout << "int " << sizeof(int) << " bytes." << endl;
+    cout << "long " << sizeof(long) << " bytes." << endl;
+}
---
+++ playground/test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    extern int kl;
+    int ival = 42;
+    // int *p = &ival;
+
+    int i = 42;
+    int j = 13;
+    int *p = &j;
+
+    
+    cout << p <<endl; //0x61ff00
+    cout << *p <<endl; //13
+
+    int *&r = p;
+
+
+    
+    cout << r <<endl; //0x61ff00
+    cout << *r <<endl; //13
+    r = &i;
+    *r = 0;
+
+    cout << r <<endl; //0x61ff08
+    cout << i <<endl; //0
+
+
+
+    // cout << *r <<endl; //0x61ff08
+
+    cout << &ival <<endl; //0x61ff08
+    cout << *p << endl; //42
+    cout << ival << endl; //42
+    return 0;
+}
+
