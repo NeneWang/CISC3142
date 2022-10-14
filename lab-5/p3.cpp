0e9e5593da88081ef773522e14a3b20b753c9ca4
NelsonWang <wangnelson2@gmail.com>
2022-10-14 07:53:38 -0400

    random progress :sob:

--- lab-5/p3.cpp
+++ lab-5/p3.cpp
@@ -13,17 +13,17 @@ int main(){
 
     inputFile.open(INPUTFILENAME);
 
-    int sumX=0, sumY=0, countLines=0;
-    vector<int> xarr, yarr;
-    int x, y;
+    double sumX=0, sumY=0, countLines=0;
+    vector<double> xarr, yarr;
+    double x, y;
     string singleLine;
 
     if(inputFile.is_open()){
         while(inputFile){
-            getline(inputFile, cin);
+            getline(inputFile, singleLine);
             // singleLine >> x >> y; // I guess this doesn't work, Research how to use cin for string
-            cin >> x >> y;
-            cout << x << " " << y;
+            sscanf(singleLine, "%f %f", x, y);
+            printf("%f %f", x, y)
 
 
             sumX += x;
