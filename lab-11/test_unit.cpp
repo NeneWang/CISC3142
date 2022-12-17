d3c2105e46be07cc49ce387c0f3a0031b7fdf72d
NelsonWang <wangnelson2@gmail.com>
2022-12-17 17:14:14 -0500

    random progress :sob:

--- lab-11/test_unit.cpp
+++ lab-11/test_unit.cpp
@@ -226,6 +226,21 @@ void nonManagedFoo(void)
     pi.getX();
 }
 
+template <typename T>
+T myMax(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+void templateExample(void)
+{
+    cout << myMax<int>(3, 7) << endl; // Call myMax for int
+    cout << myMax<double>(3.0, 7.0)
+         << endl; // call myMax for double
+    cout << myMax<char>('g', 'e')
+         << endl; // call myMax for char
+}
+
 TEST_LIST = {
     // {"askdynamic", askdynamic},
     // {"test_shared", test_shared},
