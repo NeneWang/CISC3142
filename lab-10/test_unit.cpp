5330d440bb5205094243c6d852c77f73601fba5e
NelsonWang <wangnelson2@gmail.com>
2022-12-18 01:17:53 -0500

    template utils :woman_pilot:

--- lab-10/test_unit.cpp
+++ lab-10/test_unit.cpp
@@ -38,52 +38,57 @@ void problem1(void)
     const string FILEPATH = "film.csv.txt";
     MovieProcessor movieProcessor(2);
     movieProcessor.read_csv(FILEPATH);
-    
+
     vector<MovieInformation> movieVect = movieProcessor.movieCollection;
 
     int sumLengths = accumulate(begin(movieVect), end(movieVect), 0, [](const int &x, const MovieInformation &y)
-                             { return x + y.length; });
+                                { return x + y.length; });
 
     printf("\nSum of popularity using accumulate isis %d", sumLengths);
     TEST_ASSERT(sumLengths == movieProcessor.totalFilmLength);
-    
 }
 
-void problem2(void){
+void problem2(void)
+{
 
     const string FILEPATH = "film.csv.txt";
     MovieProcessor movieProcessor(2);
     movieProcessor.read_csv(FILEPATH);
-    
+
     vector<MovieInformation> movieVect = movieProcessor.movieCollection;
 
-
-    sort(movieVect.begin(), movieVect.end(), [](MovieInformation a, MovieInformation b) {
-        return a.length > b.length;
-    });
+    sort(movieVect.begin(), movieVect.end(), [](MovieInformation a, MovieInformation b)
+         { return a.length > b.length; });
 
     // Check if they are sorted by length
     // int INT_MAX = 1000000000;
     int prev = INT_MAX;
-    for (MovieInformation mvInfo: movieVect){
+    for (MovieInformation mvInfo : movieVect)
+    {
         TEST_ASSERT(mvInfo.length <= prev);
         prev = mvInfo.length;
     }
+
+    printf("\n%d minutes long - %s", movieVect.at(0).length, movieVect.at(0).title.c_str());
+    printf("\n%d minutes long - %s", movieVect.at(1).length, movieVect.at(1).title.c_str());
+    printf("\n%d minutes long - %s", movieVect.at(2).length, movieVect.at(2).title.c_str());
 }
 
-void problem3(void){
+void problem3(void)
+{
 
     // Searching
     const string FILEPATH = "film.csv.txt";
     MovieProcessor movieProcessor(2);
     movieProcessor.read_csv(FILEPATH);
-    
+
     vector<MovieInformation> movieVect = movieProcessor.movieCollection;
     // Godfather, The
     string title_search = "Godfather, The";
 
     vector<string> titles;
-    for (MovieInformation movieInformation : movieVect){
+    for (MovieInformation movieInformation : movieVect)
+    {
         titles.push_back(movieInformation.title);
     }
     const string SEARCH_TITLE = "Godfather, The";
@@ -93,10 +98,12 @@ void problem3(void){
     movieVect.at(position).printContent();
 
     TEST_ASSERT(movieVect.at(position).title == SEARCH_TITLE);
-
 }
 
-void problem4(void){
+
+
+void problem4(void)
+{
     // Searching
     const string FILEPATH = "film.csv.txt";
     MovieProcessor movieProcessor(2);
@@ -106,39 +113,45 @@ void problem4(void){
     vector<MovieInformation> movieVect = movieProcessor.movieCollection;
     vector<int> actionMovies, comedyMovies;
     string category_1 = "Action", category_2 = "Comedy";
-    for (MovieInformation movie : movieVect){
-        if(movie.subject == category_1){
+    for (MovieInformation movie : movieVect)
+    {
+        if (movie.subject == category_1)
+        {
+            // cout << movie.year << endl;
             actionMovies.push_back(movie.year);
             // movie.printContent();
         }
-        else if(movie.subject == category_2){
-            actionMovies.push_back(movie.year);
+        else if (movie.subject == category_2)
+        {
+            comedyMovies.push_back(movie.year);
             // movie.printContent();
         }
-
     }
 
     sort(actionMovies.begin(), actionMovies.end());
     sort(comedyMovies.begin(), comedyMovies.end());
 
-    auto last = unique(actionMovies.begin(),actionMovies.end());
-    auto last_2 = unique(comedyMovies.begin(),comedyMovies.end());
+    auto last = unique(actionMovies.begin(), actionMovies.end());
+    auto last_2 = unique(comedyMovies.begin(), comedyMovies.end());
 
     actionMovies.erase(last, actionMovies.end());
     comedyMovies.erase(last_2, comedyMovies.end());
-    // for (auto year : actionMovies){
-    //     cout << year << endl;
-    // }
+
+    cout << "\n Action Movies: \n" << endl;
+    printFirsts(actionMovies, 3);
+
+    cout << "\n Comedy Movies: \n" << endl;
+    printFirsts(comedyMovies, 3);
 
     bool result = equal(comedyMovies.begin(), comedyMovies.end(), actionMovies.begin());
-    if(result){
+    if (result)
+    {
         cout << "\nAll the years in Action Movie appears in Comdy Movies";
-    }else{
-        cout << " They dont have years in common";
     }
-    
-
-
+    else
+    {
+        cout << " They dont have all the years in common";
+    }
 }
 
 TEST_LIST = {
--- lab-10/utils.cpp
+++ lab-10/utils.cpp
@@ -3,9 +3,21 @@
 
 using namespace std;
 
+template <typename T>
+void loop_vect(vector<T> vect)
+{
+    for (auto val : vect)
+    {
+        cout << val << endl;
+    }
+}
 
-void loop_vect(vector<int> vect){
-    for(auto val : vect){
-        cout << val;
+template <typename T>
+void printFirsts(vector<T> arr, int to_print)
+{
+
+    for (int i = 0; i < to_print; i++)
+    {
+        printf("\n%d=%s", i, to_string(arr.at(i)).c_str());
     }
 }
